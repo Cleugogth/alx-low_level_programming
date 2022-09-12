845a2e0f0f51ba40444d138b3694187b0792f68d
Cleugogth <princewillonyemachi101@gmail.com>
2022-09-12 10:35:09 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,9 +8,7 @@
  */
 int main(void)
 {
-	
-	char *s = "Programming is like building a multilingual puzzle";
-	puts(s);
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
