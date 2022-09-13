d9eb63aae6a1027b8223c2b3b1d686b838eb23e9
Cleugogth <princewillonyemachi101@gmail.com>
2022-09-12 18:35:27 -0700

    Hello,printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,8 +8,8 @@
  */
 int main(void)
 {
-  	printf("\n with proper grammar, but the outcome is a piece of art, followed by a new line");
+	printf("\n with proper grammar, but the outcome is a piece of art,");
 
-	return 0;
+	return (0);
 }
 
