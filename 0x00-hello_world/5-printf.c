676c404b2726c209a40ee9720b53d4e8bd0854a6
Cleugogth <princewillonyemachi101@gmail.com>
2022-09-12 18:00:11 -0700

    Hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	// c program that prints using printf  
+  	printf("\n with proper grammar, but the outcome is a piece of art, followed by a new line");
+
+	return 0;
+}
+
